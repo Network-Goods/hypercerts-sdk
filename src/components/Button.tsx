9b641c12074110bd93fa05b2262d1a3a57138d02
bitbeckers <code@bitbeckers.com>
2023-01-19 21:30:55 +0100

    feat(state): added state to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 type ButtonProps = { message: string };
 
 export const Button: React.FC<ButtonProps> = ({ message }) => {
-  return <div onClick={() => console.log(`Message: ${message}`)}>{message}</div>;
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => {
+    console.log(`Message: ${message}`);
+    setCount(count + 1);
+  };
+  return <button onClick={handleClick}>{`Message: ${message} and count: ${count}`}</button>;
 };
